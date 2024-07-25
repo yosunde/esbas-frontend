17747e07aa167e05d8fa1f9a9246f6132f9a5431
yosunde <147327765+yosunde@users.noreply.github.com>
2024-07-25 16:19:14 +0300

    Update AddNewEvent.js

--- frontend/src/components/AddNewEvent.js
+++ frontend/src/components/AddNewEvent.js
@@ -44,14 +44,10 @@ const AddNewEvent = () => {
     navigate(path);
   };
 
-  const handleLoGoClick =()=>{
-  navigate("/");
-  }
-
   return (
     <div className="add-new-event-container">
       <header className="header">
-        <img src="./logo-esbas.png" onClick={handleLoGoClick} className="logo" alt="logo" />
+        <img src="./logo-esbas.png" className="logo" alt="logo" />
       </header>
       <div className="add-new-event">
         <h1>Yeni Etkinlik Oluşturma</h1>
@@ -118,4 +114,6 @@ const AddNewEvent = () => {
 };
 
 export default AddNewEvent;
+
+
 
